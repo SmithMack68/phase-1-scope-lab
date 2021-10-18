d6955cf9c008ce9592698bb3d88a506e8be88045
Candice Smith MacKenzie <Mahony8@aol.com>
2021-10-18 13:30:33 -0400

    commit tree a0396f106b314f2564dc53e5c6f58ebb83bffe1b

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'Scott'
+
+function upperCaseCustomerName(){
+    customerName = 'BOB';
+}
+
+function setBestCustomer(){
+   return bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = 'It is Scott'
+}
